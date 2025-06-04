5c1cbbec41b2cf7aba9d96e4b58a996c16f05b45
Ilham Hafidz <74355067+ilhamhafidz404@users.noreply.github.com>
2025-06-04 16:36:28 +0700

    Basic Control Playable Character

---
+++ Assets/Sprites/Characters/Player/Scripts/BasicController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicController : MonoBehaviour
+{
+
+    // BASIC COMPONENTS
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D cc;
+
+    // Preparation for Walk & Run Character
+    Vector2 v2;
+
+    [SerializeField]
+    private bool isMoving = false;
+    public bool IsMoving
+    {
+        get { return isMoving; }
+        set { 
+            isMoving = value; 
+            anim.SetBool("isMoving", value);
+        }
+    }
+
+    private bool isRunning = false;
+    public bool IsRunning {
+        get { return isRunning; }
+        set {
+            isRunning = value;
+            
+            anim.SetBool("isRunning", value);
+        }
+    }
+
+    public float walkSpeed = 2f;
+    public float runSpeed = 5f;
+
+    public float currentSpeed {
+        get {
+            float speed = 0f;
+            if(isRunning && IsMoving){
+                speed = runSpeed;
+            } else if(IsMoving) {
+                speed = walkSpeed;
+            } else {
+                speed = 0f;
+            }
+
+            return speed;
+        }
+    }
+
+    // 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        cc = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(v2.x * currentSpeed, rb.linearVelocity.y);
+    }
+
+
+    // Listen Input Move
+    public void onMove(InputAction.CallbackContext context){
+        v2 =  context.ReadValue<Vector2>();
+
+        IsMoving = v2 != Vector2.zero;
+
+        if (v2.x != 0) {
+            spriteRenderer.flipX = v2.x < 0;
+        }
+    }
+
+    // Listen Input Move + Run
+    public void onRunning(InputAction.CallbackContext context){
+        if (context.started){
+            IsRunning = true;
+        } else if (context.canceled){
+            IsRunning = false;
+        }
+    }
+}
